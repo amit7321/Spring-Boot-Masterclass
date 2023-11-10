8bfe4381eecfd35044d875a6a5c28a9093972bdd
amit <amit.hasan7321@gmail.com>
2023-11-11 03:10:18 +0600

    Implement logging and actuator

--- src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
@@ -1,12 +1,16 @@
 package com.example.springbootmasterclass.customer;
 
  import com.example.springbootmasterclass.exception.NotFoundException;
-import org.springframework.stereotype.Service;
+ import org.slf4j.Logger;
+ import org.slf4j.LoggerFactory;
+ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger logger = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     public CustomerService(CustomerRepository customerRepository) {
@@ -14,11 +18,15 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        logger.info("getCustomer is called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with " + id + " not found"));
+                .orElseThrow(() -> {
+                    logger.error("eeeeerrrrrrrrrrroooooooor");
+                    return new NotFoundException("customer with " + id + " not found");
+                });
     }
 }
